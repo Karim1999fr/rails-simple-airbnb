83586fa111d57b80bcb63faec7d194bdd6024dc2
Karim Abouchalache <kfa4vw@virginia.edu>
2019-11-13 16:51:29 +0100

    Day 28 Exercice 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!([{
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+    name: 'Dark & Spacious Garden Flat London',
+    address: '15 Clifton Gardens London W9 1DT',
+    description: 'A lovely winter feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 72,
+    number_of_guests: 4
+  },
+  {
+    name: 'Clear & Spacious Garden Flat London',
+    address: '12 Clifton Gardens London W9 1DT',
+    description: 'A lovely spring feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 80,
+    number_of_guests: 5
+  },
+  {
+    name: 'Green & Spacious Garden Flat London',
+    address: '13 Clifton Gardens London W9 1DT',
+    description: 'A lovely fall feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 85,
+    number_of_guests: 6
+  }
+])
